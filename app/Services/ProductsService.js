3d28d8492ca9adde87b2edfd55af1d1e2ce8eb14
NiceJared <jaredvande@gmail.com>
2021-04-01 20:04:53 -0600

    first commit

---
+++ app/Services/ProductsService.js
@@ -0,0 +1,11 @@
+import { ProxyState } from "../AppState.js";
+import Product from "../Models/Product.js";
+
+class ProductsService {
+
+}
+
+
+
+export const productsService = new ProductsService();
+
